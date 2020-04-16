75939e416d18dafb47aec5b4f2356a0cbbcd774c
Drew <djnevins@uw.edu>
2020-04-16 18:09:32 -0400

    added random num generator and variable to hold

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let userName = ""
 userName ? console.log(`Hello, ${userName}!`) : console.log('false');
+
 let userQuestion = "What should I do tonight"
 console.log(`${userQuestion}, ${userName}?`)
+
+let randomNumber = Math.floor(math.random() * 8);
+
+let eightBall = ""
+
+
