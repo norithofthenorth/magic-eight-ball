7ba71700d218abc1d65927b2c4a691edfe9e3e09
Drew <djnevins@uw.edu>
2020-04-16 18:17:03 -0400

    added magic eight ball questions

--- script.js
+++ script.js
@@ -8,4 +8,39 @@ let randomNumber = Math.floor(math.random() * 8);
 
 let eightBall = ""
 
+switch (condition) {
+    case 0: 
+    eightBall = 'It is certain';
+    break;
+
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+        
+        case 2:
+            eightBall = 'Reply hazy try again';
+            break;
+
+            case 3:
+                eightBall = 'Cannot predict now';
+                break;
+
+                case 4:
+                    eightBall = 'Do not count on it';
+                    break;
+
+                    case 5:
+                        eightBall = 'My sources say no';
+                        break;
+
+                        case 6:
+                            eightBall = 'Outlook not so good';
+                            break;
+
+                            case 7:
+                                eightBall = 'Signs point to yes';
+                                break;
+
+}
+
 
